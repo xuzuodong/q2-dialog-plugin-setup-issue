e43991e21c73c3bb0134b41651d2f10f77e1bfd7
许作栋 <xzd333333@163.com>
2021-05-20 14:34:25 +0800

    chore: change name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,18 @@
 <template>
     <q-page class="flex flex-center">
-        <q-btn @click="openSetupDialog" label="<setup> dialog" no-caps class="q-mr-md" />
+        <q-btn @click="openScriptDialog" label="<script> dialog" no-caps class="q-mr-md" />
         <q-btn @click="openScriptSetupDialog" label="<script setup> dialog" no-caps />
     </q-page>
 </template>
 
 <script setup lang="ts">
 import { useQuasar } from 'quasar'
-import SetupDialog from './SetupDialog.vue'
+import SetupDialog from './ScriptDialog.vue'
 import ScriptSetupDialog from './ScriptSetupDialog.vue'
 
 const quasar = useQuasar()
 
-function openSetupDialog() {
+function openScriptDialog() {
     quasar.dialog({
         component: SetupDialog,
     })
